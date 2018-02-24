93489bdb6acc4a93328d0379da25b3066817a0f0
Seanlangbrown <seanlangbrown@gmail.com>
2018-02-23 17:23:32 -0800

    update server port configuration

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const path = require('path');
-const port = 8080;
+const port = 3001; // process.env.PORT ||
 
 const app = express();
 

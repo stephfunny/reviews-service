8ac17b1807a2d62b5af116af636629431bf987a9
Seanlangbrown <seanlangbrown@gmail.com>
2018-02-26 14:44:37 -0800

    seperate server app from server to allow unit testing without using a port

--- server/server.js
+++ server/server.js
@@ -3,13 +3,8 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const path = require('path');
 const port = 3001; // process.env.PORT ||
-
-const app = express();
+const app = require('server-app.js');
 
 app.use(morgan('dev'));
-
-app.get('/', (req, res) => (res.sendFile(path.join(__dirname, '..', 'client', 'public', 'index.html'))));
-
-app.use('/content', express.static(path.join(__dirname, '..','client','public')));
 
 app.listen(port, () => (console.log('reviews microservice running at 127.0.0.1:' + port)));

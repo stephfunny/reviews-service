713df52010f121aee603a56f30d1dee84947bd36
Seanlangbrown <seanlangbrown@gmail.com>
2018-02-26 16:26:07 -0800

    complete unit tests for Express server endpoints

---
+++ server/server.test.js
@@ -0,0 +1,28 @@
+const serverApp = require('./server-app.js');
+const supertest = require('supertest');
+const path = require('path');
+const fs = require('fs');
+
+
+
+describe('Express Server Endpoints', () => {
+  test('It should respond to the root path', async () => {
+    const response = await supertest(serverApp).get('/');
+    expect(response.statusCode).toBe(200);
+  });
+
+  test('It should respond with an error to invalid paths', async () => {
+    const response = await supertest(serverApp).get('/baloney');
+    expect(response.statusCode).toBe(404);
+  });
+
+  test('It should respond to static file requests from the public directory', async () => {
+
+    const staticResponse = await supertest(serverApp).get('/content/index.test.html');
+    const expectedResponse = fs.readFileSync(path.join(__dirname, '../client/public/index.test.html'), 'utf8');
+    expect(staticResponse.statusCode).toBe(200);
+    expect(staticResponse.text).toEqual(expectedResponse);
+  })
+
+
+})

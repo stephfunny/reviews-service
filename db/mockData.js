6eca403ed5555e7749f93f2acde1c5e8565d592d
Seanlangbrown <seanlangbrown@gmail.com>
2018-02-27 15:29:06 -0800

    debug mockData

--- db/mockData.js
+++ db/mockData.js
@@ -3,27 +3,27 @@ const reviews = [
     id: 1,
     numberOfReviews: 3,
     averageRating: 5,
-    rating-accuracyDesc: 4.3,
-    rating-communication: 2.3,
-    rating-cleanliness: 0,
-    rating-location: 4.5,
-    rating-CheckIn: 3.0,
-    rating-Value: 3.3,
+    ratingAccuracyDesc: 4.3,
+    ratingCommunication: 2.3,
+    ratingCleanliness: 0,
+    ratingLocation: 4.5,
+    ratingCheckIn: 3.0,
+    ratingValue: 3.3,
     reviews: [
       {
-        user-img: 'string',
+        userImg: 'string',
         user: 'user1',
         date: 'Mar 2017',
         comment: 'Super nice people'
       }, 
       {
-        user-img: 'string',
+        userImg: 'string',
         user: 'user2',
         date: 'Feb 2018',
         comment: 'comment about experience #1'
       }, 
       {
-        user-img: 'string',
+        userImg: 'string',
         user: 'user3',
         date: 'Jan 2018',
         comment: 'Would do it again'
@@ -35,27 +35,27 @@ const reviews = [
     id: 2,
     numberOfReviews: 3,
     averageRating: 5,
-    rating-accuracyDesc: 4.3,
-    rating-communication: 5.0,
-    rating-cleanliness: 0,
-    rating-location: 4.5,
-    rating-CheckIn: 3.0,
-    rating-Value: 3.3,
+    ratingAccuracyDesc: 4.3,
+    ratingCommunication: 5.0,
+    ratingCleanliness: 0,
+    ratingLocation: 4.5,
+    ratingCheckIn: 3.0,
+    ratingValue: 3.3,
     reviews: [
       {
-        user-img: 'string',
+        userImg: 'string',
         user: 'user1',
         date: 'Mar 2017',
         comment: 'comment about experience #2'
       }, 
       {
-        user-img: 'string',
+        userImg: 'string',
         user: 'user2',
         date: 'Dec 2017',
         comment: 'Yay so fun!'
       }, 
       {
-        user-img: 'string',
+        userImg: 'string',
         user: 'user5',
         date: 'Jan 2014',
         comment: 'BORING!!'
@@ -67,27 +67,27 @@ const reviews = [
     id: 3,
     numberOfReviews: 3,
     averageRating: 5,
-    rating-accuracyDesc: 4.3,
-    rating-communication: 2.3,
-    rating-cleanliness: 0,
-    rating-location: 4.5,
-    rating-CheckIn: 1.0,
-    rating-Value: 3.3,
+    ratingAccuracyDesc: 4.3,
+    ratingCommunication: 2.3,
+    ratingCleanliness: 0,
+    ratingLocation: 4.5,
+    ratingCheckIn: 1.0,
+    ratingValue: 3.3,
     reviews: [
       {
-        user-img: 'string',
+        userImg: 'string',
         user: 'user1',
         date: 'Mar 2017',
         comment: 'comment about experience #3'
       }, 
       {
-        user-img: 'string',
+        userImg: 'string',
         user: 'user2',
         date: 'Feb 2018',
         comment: 'I had fun'
       }, 
       {
-        user-img: 'string',
+        userImg: 'string',
         user: 'user3',
         date: 'Jan 2018',
         comment: 'not worth the money'

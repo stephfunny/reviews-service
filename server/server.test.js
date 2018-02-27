b78d16166376883aa02097c5abf05d7d0214dbc9
Seanlangbrown <seanlangbrown@gmail.com>
2018-02-26 16:33:18 -0800

    refactor tests of static files

--- server/server.test.js
+++ server/server.test.js
@@ -18,10 +18,27 @@ describe('Express Server Endpoints', () => {
 
   test('It should respond to static file requests from the public directory', async () => {
 
+    const testFilePath = path.join(__dirname, '../client/public/index.test.html');
+    let testHTML = `<!DOCTYPE html>
+      <html>
+      <head>
+        <title>TestFilr</title>
+      </head>
+      <body>
+        This is a test
+
+      </body>
+      </html>`;
+
+    
+    fs.writeFile(testFilePath, testHTML, (err) => {
+      if (err) throw err;
+      console.log('The test file could not be created in ');
+    });
     const staticResponse = await supertest(serverApp).get('/content/index.test.html');
-    const expectedResponse = fs.readFileSync(path.join(__dirname, '../client/public/index.test.html'), 'utf8');
+    //const expectedResponse = fs.readFileSync(testFilePath, 'utf8');
     expect(staticResponse.statusCode).toBe(200);
-    expect(staticResponse.text).toEqual(expectedResponse);
+    expect(staticResponse.text).toEqual(testHTML);//expectedResponse);
   })
 
 

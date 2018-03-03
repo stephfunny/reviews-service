8bc40cc74df3551e458b7fce594b8f25074f7b3c
Seanlangbrown <seanlangbrown@gmail.com>
2018-03-02 17:53:48 -0800

    implement dynamic rendering of aggregate reviews

--- client/src/reviewAggregates.jsx
+++ client/src/reviewAggregates.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
 import StarScale from './StarScale.jsx';
+import _ from 'lodash';
 
 export default class ReviewAggregates extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
   }
 
+
+  displayReview(name, rating, key) {
+    return (
+    <div key={key}>
+      <div>
+       <span>{name}</span>
+      </div>
+      <div>
+        <StarScale rating={rating} />
+      </div>
+    </div>
+    );
+  }
 
 
   render () {
 
     return (
       <div id="reviewaggeegates">
-            Cleanliness <StarScale rating="1" /> Accuracy <StarScale rating="3" />
-            Location Check In
-          </div>
-      );
+        {_.map(this.props.aggregateReviews, (review, i) => (this.displayReview(review.name, review.rating, i)))}
+      </div>
+    );
   }
 }

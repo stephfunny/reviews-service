166776b29812f09c23ef79d6fd6be512b4aac403
Seanlangbrown <seanlangbrown@gmail.com>
2018-03-05 10:25:49 -0800

    implement input in searchBox

--- client/src/reviewsHeader.jsx
+++ client/src/reviewsHeader.jsx
@@ -18,15 +18,17 @@ export default class ReviewHeader extends React.Component {
           <div>
             <h4>
               <span>
-                <span>{this.props.reviews} REVIEWS</span>
+                <span>{this.props.reviews} Reviews</span>
               </span>
               <StarScale rating={this.props.overallRating} height="30px" width="120px" />
             </h4>
           </div>
-            <SearchBox text={this.props.searchTerms} submitQuery={this.props.submitQuery} />
+            <SearchBox text={this.props.searchTerms} submitQuery={this.props.submitQuery} searchTerms={this.props.searchTerms} />
         </div>
         <div style={{marginTop:'16px', marginBottom:'16px'}} >
-          <div className="divider"></div>
+          <div className="divider">
+            <hr/>
+          </div>
         </div>
       </div>
       );

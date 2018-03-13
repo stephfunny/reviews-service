4418b28abadc4bf6894b171a5fefe87cd9b15eec
Seanlangbrown <seanlangbrown@gmail.com>
2018-03-13 13:25:00 -0700

    implement zagat -like styling and UI changes

--- client/src/reviewsHeader.jsx
+++ client/src/reviewsHeader.jsx
@@ -16,12 +16,18 @@ export default class ReviewHeader extends React.Component {
     return (
       <div id="reviews-header" className={styles.reviewsHeader}>
         <div id="reviews-header-overview" className={styles.overview} >
-          <SearchBox className={styles.searchBox} text={this.props.searchTerms} submitQuery={this.props.submitQuery} searchTerms={this.props.searchTerms} />
-          <div id="reviews-header-title" className={styles.title} >
-            <h2 className={styles.reviewsCount}>{this.props.reviews + ' Reviews'}</h2>
+          {/*<SearchBox className={styles.searchBox} text={this.props.searchTerms} submitQuery={this.props.submitQuery} searchTerms={this.props.searchTerms} />*/}
+          <div id="reviews-header-title" className={styles.titleRating} >
+            <h4 className={styles.googleReviews}>Google Reviews</h4>
+          </div>
+          <div id="reviews-header-title-rating" className={styles.title} >
+            <h4 className={styles.titleRating}>{this.props.overallRating + '.0'}</h4>
           </div>
           <div id="reviews-header-rating" className={styles.rating} >
             <StarScale className={styles.starScale} rating={this.props.overallRating} height="30px" width="120px" />
+          </div>
+          <div id="reviews-header-title" className={styles.title} >
+            <h4 className={styles.reviewsCount}>{'(' + this.props.reviews + ')'}</h4>
           </div>
         </div>
         <hr className={styles.hr} />
